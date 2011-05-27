7d4da42dfe6a937a4920e73e902931521029734f
Freddy Mercury <root@splainhost.com>
2011-05-27 18:46:50 +0400

    Captcha for contactus.php

--- contactus.php
+++ contactus.php
@@ -9,16 +9,12 @@ if (AuthController::getInstance()->isAuthorized()) {
 }
 include_once(LIB_ROOT.'/recaptchalib.php');
 if (isset($_REQUEST['action']) && $_REQUEST['action'] == 'send') {
-	if (!isset($_SESSION['captcha'])) {
-		$privatekey = "6LcNwsQSAAAAAMKYE64_1FChTxf1MkF5OQBNm-t7";
-		$resp = recaptcha_check_answer ($privatekey, $_SERVER["REMOTE_ADDR"], $_POST["recaptcha_challenge_field"], $_POST["recaptcha_response_field"]);
-		if (!$resp->is_valid) {
-		  location($_SERVER['PHP_SELF'], '<p  style="color:red; font-weight:bold;">The CAPTCHA wasn\'t entered correctly. Go back and try it again.</p>');
-		}
-		$_SESSION['captcha'] = true;
+	$privatekey = "6LcNwsQSAAAAAMKYE64_1FChTxf1MkF5OQBNm-t7";
+	$resp = recaptcha_check_answer ($privatekey, $_SERVER["REMOTE_ADDR"], $_POST["recaptcha_challenge_field"], $_POST["recaptcha_response_field"]);
+	if (!$resp->is_valid) {
+	  location($_SERVER['PHP_SELF'], '<p  style="color:red; font-weight:bold;">The CAPTCHA wasn\'t entered correctly. Go back and try it again.</p>');
 	}
 	if (empty($_POST['subject']) || empty($_POST['message']) || (empty($_POST['email']) && !$user->id)) {
-		$_SESSION['captcha'] = false;
 		location($_SERVER['PHP_SELF'], '<p  style="color:red; font-weight:bold;">All fields required!</p>');
 	}
 	else {
@@ -28,7 +24,6 @@ if (isset($_REQUEST['action']) && $_REQUEST['action'] == 'send') {
 		$emailer->subject = 'From: '.($user->id ? $user->login : $emailer->from).' -> '.$_POST['subject'];
 		$emailer->body = $_POST['message'];
 		$emailer->sendToSystem();
-		$_SESSION['captcha'] = false;
 		location($_SERVER['PHP_SELF'], '<p  style="color:red; font-weight:bold;">Your message has been sent to support!</p>');
 	}
 }
