e6da5726a47784a79396939bb0f041b4605db7e5
Freddy Mercury <root@splainhost.com>
2011-05-27 16:41:15 +0400

    Captcha for contactus.php

--- contactus.php
+++ contactus.php
@@ -7,8 +7,16 @@ if (AuthController::getInstance()->isAuthorized()) {
 	$user = new User(Project::getInstance()->getCurUser()->id);
 	Project::getInstance()->getSmarty()->assign('user', $user);
 }
-
+include_once(LIB_ROOT.'/recaptchalib.php');
 if (isset($_REQUEST['action']) && $_REQUEST['action'] == 'send') {
+	if (!isset($_SESSION['captcha'])) {
+		$privatekey = "6LcNwsQSAAAAAMKYE64_1FChTxf1MkF5OQBNm-t7";
+		$resp = recaptcha_check_answer ($privatekey, $_SERVER["REMOTE_ADDR"], $_POST["recaptcha_challenge_field"], $_POST["recaptcha_response_field"]);
+		if (!$resp->is_valid) {
+		  location($_SERVER['PHP_SELF'], '<p  style="color:red; font-weight:bold;">The CAPTCHA wasn\'t entered correctly. Go back and try it again.</p>');
+		}
+		$_SESSION['captcha'] = true;
+	}
 	if (empty($_POST['subject']) || empty($_POST['message']) || (empty($_POST['email']) && !$user->id)) {
 		location($_SERVER['PHP_SELF'], '<p  style="color:red; font-weight:bold;">All fields required!</p>');
 	}
@@ -22,4 +30,6 @@ if (isset($_REQUEST['action']) && $_REQUEST['action'] == 'send') {
 		location($_SERVER['PHP_SELF'], '<p  style="color:red; font-weight:bold;">Your message has been sent to support!</p>');
 	}
 }
+
+Project::getInstance()->getSmarty()->assign('captcha', recaptcha_get_html('6LcNwsQSAAAAAGlQU25aUQDDUs1wFbv_j3XHZZ8b'));
 Project::getInstance()->showPage('contactus.tpl');
