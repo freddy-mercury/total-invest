f778eee394ca66984d20bb6bf269b594308ecefb
Freddy Mercury <root@splainhost.com>
2011-06-08 09:20:57 +0400

    Security fix

--- includes/inlines/check_login.php
+++ includes/inlines/check_login.php
@@ -3,7 +3,7 @@ $ACCESS_LEVEL = ACCESS_LEVEL_GUEST;
 include_once(DOC_ROOT.'/includes/authorization.php');
 if (!empty($_REQUEST['login'])) {
 	include_once(LIB_ROOT.'/users/user.class.php');
-	if (User::loginExist($_REQUEST['login'])) {
+	if (User::loginExist(sql_escapeStr($_REQUEST['login']))) {
             echo '<br /><span style="color:red;"><small>This login is not free</small></span>';
 	}
         else {
